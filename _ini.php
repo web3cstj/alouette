057085499c6312eecaf602bb52a71291e5cc133d
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-01-23 23:32:24 -0500

    Changement de catégorie

--- _ini.php
+++ _ini.php
@@ -5,19 +5,20 @@ $ini = array (
 	'titre' => 'Alouette, gentille Alouette',
 
 	//Categories sous forme "Catégorie/Sous-Catégorie/..."
-	'categorie' => 'Exemples, exercices/Cours 03',
+	'categorie' => 'Exemples, exercices/Cours 02',
 
 	//Un préfixe à mettre devant le titre pour le tri "Cours 01 : "
 	'prefixe' => '',
 
 	//Un tableau de liens (étiquette=>url) ou une série de lignes (étiquette=url)
-	'liens' => array (),
+	'liens' => array (
+),
 
 	//Booléen. Doit-on afficher la source?
 	'source' => true,
 
 	//Booléen. Le dossier est-il visible dans la liste? Il reste tout de même accessible.
-	'visible' => true,
+	'visible' => false,
 
 	//Booléen. Y a-t-il des fichiers à télécharger?
 	'fichiers' => false,
