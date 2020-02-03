c79bfe2d1ed5d779fa25bbf9d96958c7e70cf193
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-02-03 10:08:25 -0500

    Version eleve

--- Alouette.php
+++ Alouette.php
@@ -6,9 +6,8 @@ class Alouette {
 	 * @param string $qualite - La qualité que l'on donne à l'oiseau (gentille)
 	 * @return string
 	 */
-	static public function titre($oiseau, $qualite) {
-		return $oiseau.', '.$qualite.' '.$oiseau;
-	}
+
+
 	/** Méthode chanson
 	 * Retourne la chanson au complet dans un div.chanson
 	 * @param string $oiseau - Le nom de l'oiseau (Alouette)
@@ -18,17 +17,8 @@ class Alouette {
 	 * @uses strophe
 	 * @return string
 	 */
-	static public function chanson($oiseau, $qualite, $action, $membres) {
-		$resultat = '';
-		$membresDits = array();
-		$resultat .= '<div class="chanson">';
-		foreach ($membres as $membre) {
-			array_unshift($membresDits, $membre);
-			$resultat .= Alouette::strophe($oiseau, $qualite, $action, $membre, $membresDits);
-		}
-		$resultat .= '</div>';
-		return $resultat;
-	}
+	
+	
 	/** Méthode strophe
 	 * Retourne une strophe au complet
 	 * @param string $oiseau - Le nom de l'oiseau (Alouette)
@@ -40,14 +30,8 @@ class Alouette {
 	 * @uses couplet
 	 * @return string
 	 */
-	static public function strophe($oiseau, $qualite, $action, $membre, $membresDits) {
-		$resultat = '';
-		$resultat .= '<div class="strophe">';
-		$resultat .= self::refrain($oiseau, $qualite, $action);
-		$resultat .= self::couplet($oiseau, $action, $membre, $membresDits);
-		$resultat .= '</div>';
-		return $resultat;
-	}
+	
+	
 	/** Méthode refrain
 	 * Retourne les 2 phrases du refrain dans des div. Le tout répété 2 fois à l'aide
 	 *   de appelReponse et regroupé dans un dans un div.refrain
@@ -58,14 +42,8 @@ class Alouette {
 	 * @uses appelReponse
 	 * @return string
 	 */
-	static public function refrain($oiseau, $qualite, $action) {
-		$resultat = '';
-		$resultat .= '<div>'.self::titre($oiseau, $qualite).'</div>';
-		$resultat .= '<div>'.$oiseau.', je '.$action.'.'.'</div>';
-		$resultat = self::appelReponse($resultat);
-		$resultat = '<div class="refrain">'.$resultat.'</div>';
-		return $resultat;
-	}
+	
+
 	/** Méthode couplet
 	 * Génère l'appel puis la réponse d'une phrase de la chanson passée en paramètre.
 	 * @param string $oiseau - Le nom de l'oiseau (Alouette)
@@ -78,39 +56,24 @@ class Alouette {
 	 * @return string
 	 * @note L'appel est dans un div.appel et la réponse, dans un div.reponse
 	 */
-	static public function couplet($oiseau, $action, $membre, $membresDits) {
-		$resultat = '';
-		$resultat .= self::actionMembre($action, $membre);
-		$resultat .= self::enumMembres($membresDits);
-		$resultat .= self::appelReponse($oiseau);
-		$resultat .= '<div>Aaaah . . . </div>';
-		return $resultat;
-	}
-	/** Méthode appelReponse
+	
+	
+	 /** Méthode appelReponse
 	 * Génère l'appel puis la réponse d'une phrase de la chanson passée en paramètre.
 	 * @param string $phrase - La phrase à répéter
 	 * @return string
 	 * @note L'appel est dans un div.appel et la réponse, dans un div.reponse
 	 */
-	static public function appelReponse($phrase) {
-		$resultat = '';
-		$resultat .= '<div class="appel">'.$phrase.'</div>';
-		$resultat .= '<div class="reponse">'.$phrase.'</div>';
-		return $resultat;
-	}
+
+
 	/** Méthode enumMembres
 	 * Génère l'énumération des membres passée en paramètre.
 	 * @param array $membres - Les membres à énumérer
 	 * @uses appelReponse
 	 * @return string
 	 */
-	static public function enumMembres($membres) {
-		$resultat = '';
-		foreach ($membres as $membre) {
-			$resultat .= self::appelReponse('Et '.$membre);
-		}
-		return $resultat;
-	}
+
+	
 	/** Méthode actionMembre
 	 * Génère la menace faite à l'oiseau sur son membre.
 	 * @param string $action - L'action
@@ -118,8 +81,6 @@ class Alouette {
 	 * @uses appelReponse
 	 * @return string
 	 */
-	static public function actionMembre($action, $membre) {
-		$resultat = self::appelReponse('Je '.$action.' '.$membre);
-		return $resultat;
-	}
+
+
 }
