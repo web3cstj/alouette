1710677544cfffa8a974f559c2031d4ffd24ce5d
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-02-03 09:38:01 -0500

    Avant publication

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+Ce fichier est ajouté parce que le site sera placé sur le serveur 
+dans un sous-dossier plutôt qu'à la racine.
+NE PAS MODIFIER
+*/
+header("location:public/index.php");
+exit;
