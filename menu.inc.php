08f4198d88444e5fae45168bc87c8db5d18f4e16
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-02-24 11:21:30 -0500

    temp

--- menu.inc.php
+++ menu.inc.php
@@ -1,23 +1,27 @@
 <?php
-$elements = [
-	'Alouette' => 'index.php',
-	'Corbeau' => 'corbeau.php',
-	'Pie' => 'pie.php',
-	'Ma chanson<br/>à moi' => 'perso.php',
-	'Composer<br/>ma chanson' => 'formulaire.php',
-];
-$affichage = '';
-$affichage .= '<nav>';
-$affichage .= '<ul>';
-foreach ($elements as $etiquette => $url) {
-	if ($url === basename($_SERVER['PHP_SELF'])) {
-		$affichage .= '<li class="courant">';
-	} else {
-		$affichage .= '<li>';
+function menu() {
+
+	$elements = [
+		'Alouette' => 'index.php',
+		'Corbeau' => 'corbeau.php',
+		'Pie' => 'pie.php',
+		'Ma chanson<br/>à moi' => 'perso.php',
+		'Composer<br/>ma chanson' => 'formulaire.php',
+	];
+	$affichage = '';
+	$affichage .= '<nav>';
+	$affichage .= '<ul>';
+	foreach ($elements as $etiquette => $url) {
+		if ($url === basename($_SERVER['PHP_SELF'])) {
+			$affichage .= '<li class="courant">';
+		} else {
+			$affichage .= '<li>';
+		}
+		$affichage .= '<a href="'.$url.'">'.$etiquette.'</a>';
+		$affichage .= '</li>';
 	}
-	$affichage .= '<a href="'.$url.'">'.$etiquette.'</a>';
-	$affichage .= '</li>';
+	$affichage .= '</ul>';
+	$affichage .= '</nav>';
+	echo $affichage;
 }
-$affichage .= '</ul>';
-$affichage .= '</nav>';
-echo $affichage;
+menu();
--- public/index.php
+++ public/index.php
@@ -1,13 +1,28 @@
 <?php
+error_reporting(E_ALL);
 include "../donnees.inc.php";
 include "../Alouette.php";
-$chanson = new Alouette($oiseau, $qualite, $action, $membres);
+$pdo = new PDO("sqlite:".__DIR__."/../database/ritournelle.sqlite");
+$stmt = $pdo->prepare("SELECT id, oiseau, qualite, action, membres FROM view_chansons ORDER BY oiseau");
+$affichage = '';
+$stmt->execute();
+// var_dump($pdo->errorInfo());
+// var_dump($chanson);
+$affichage .= '<ul class="cadre">';
+while (($chanson = $stmt->fetchObject()) !== false) {
+	$affichage .= '<li>';
+	$affichage .= '<a href="corbeau.php?id='.$chanson->id.'">';
+	$affichage .= ucfirst($chanson->qualite).' '.$chanson->oiseau.', je '.$chanson->action;
+	$affichage .= '</a>';
+	$affichage .= '</li>';
+}
+$affichage .= '</ul>';
 ?><!DOCTYPE html>
 <html lang="fr">
 	<head>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="css/alouette.css" />
-		<title><?php echo $chanson->titre($oiseau, $qualite); ?></title>
+		<title>Les chansons</title>
 	</head>
 	<body>
         <div id="app">
@@ -15,8 +30,9 @@ $chanson = new Alouette($oiseau, $qualite, $action, $membres);
             <?php include "../footer.inc.php"; ?>
             <?php include "../menu.inc.php"; ?>
     		<div class="body">
-                <h1><?php echo $chanson->titre($oiseau, $qualite); ?></h1>
-                <?php echo $chanson->chanson($oiseau, $qualite, $action, $membres); ?>
+				<h1>Les chansons</h1>
+				
+				<?php echo $affichage; ?>
             </div>
         </div>
 	</body>
