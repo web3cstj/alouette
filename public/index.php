702f4d46bc64a6d9639b822a73e8f384deb2bc4c
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-02-24 11:26:03 -0500

    Faire la page publuc/index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,9 @@
 <?php
 error_reporting(E_ALL);
-include "../donnees.inc.php";
 include "../Alouette.php";
-$pdo = new PDO("sqlite:".__DIR__."/../database/ritournelle.sqlite");
-$stmt = $pdo->prepare("SELECT id, oiseau, qualite, action, membres FROM view_chansons ORDER BY oiseau");
 $affichage = '';
-$stmt->execute();
 // var_dump($pdo->errorInfo());
-// var_dump($chanson);
-$affichage .= '<ul class="cadre">';
-while (($chanson = $stmt->fetchObject()) !== false) {
-	$affichage .= '<li>';
-	$affichage .= '<a href="corbeau.php?id='.$chanson->id.'">';
-	$affichage .= ucfirst($chanson->qualite).' '.$chanson->oiseau.', je '.$chanson->action;
-	$affichage .= '</a>';
-	$affichage .= '</li>';
-}
-$affichage .= '</ul>';
+// die;
 ?><!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -31,7 +18,11 @@ $affichage .= '</ul>';
             <?php include "../menu.inc.php"; ?>
     		<div class="body">
 				<h1>Les chansons</h1>
-				
+				<ul class="cadre">
+					<li><a href="corbeau.php?id=1">Gentille Alouette, je te plumerai</a></li>
+					<li><a href="corbeau.php?id=3">Maître Corbeau, je vous charmerai</a></li>
+					<li><a href="corbeau.php?id=2">Voleuse Pie, je te pendrai</a></li>
+				</ul>
 				<?php echo $affichage; ?>
             </div>
         </div>
